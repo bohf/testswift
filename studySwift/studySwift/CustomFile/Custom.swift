627d57e3e082674b7397761ebbfcbb92a00211cd
bohf <2564403362@qq.com>
2021-11-14 18:26:25 +0800

    add user signIn page

--- studySwift/studySwift/CustomFile/Custom.swift
+++ studySwift/studySwift/CustomFile/Custom.swift
@@ -6,9 +6,39 @@
 //
 
 import UIKit
+import CommonCrypto
+
+//屏幕宽高
+let kScreenW = UIScreen.main.bounds.width
+let kScreenH = UIScreen.main.bounds.height
+
+//状态栏高度
+func getStatusBarH() -> CGFloat {
+    if #available(iOS 13.0, *) {
+        let statusBarHeight = UIApplication.shared.windows.first?.windowScene?.statusBarManager
+        return statusBarHeight?.statusBarFrame.height ?? 44
+    } else {
+        return UIApplication.shared.statusBarFrame.height
+    }
+}
+
+func viewAnimation(view: UIView, backView: UIView, backViewAlpha: CGFloat, time: TimeInterval, y: CGFloat) {
+    UIView.animate(withDuration: time) {
+        view.frame = CGRect(x: 0, y: y, width: kScreenW, height: kScreenH)
+        backView.alpha = backViewAlpha
+    }
+}
 
 extension UIColor {
     convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
         self.init(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: 1)
+    }
+}
+extension String {
+    var sha256: String {
+        let utf8 = cString(using: .utf8)
+        var digest = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+        CC_SHA256(utf8, CC_LONG(utf8!.count - 1), &digest)
+        return digest.reduce("") { $0 + String(format:"%02x", $1) }
     }
 }
--- studySwift/studySwift/ViewController/Loing/LoginViewController.swift
+++ studySwift/studySwift/ViewController/Loing/LoginViewController.swift
@@ -9,7 +9,23 @@ import UIKit
 
 class LoginViewController: UIViewController {
     var isBool = false
+    // 记录滑动Y点的值
+    var signUpViewOriginY: CGFloat!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var singInButton: UIButton!
+    private lazy var signInView: UserSignInView = {
+        let signV = UserSignInView.loadFromNib("View")
+        signV.frame = CGRect(x: 0, y: kScreenH, width: kScreenW, height: kScreenH)
+        let panView = UIPanGestureRecognizer.init(target: self, action: #selector(pan(panGuesture:)))
+        signV.addGestureRecognizer(panView)
+        return signV
+    }()
+    private lazy var grayView: UIView = {
+        let gray = UIView.init(frame: self.view.bounds)
+        gray.backgroundColor = UIColor(red: 0 / 255, green: 0 / 255, blue: 0 / 255, alpha: 0.5)
+        gray.alpha = 0
+        return gray
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
@@ -23,5 +39,45 @@ class LoginViewController: UIViewController {
     func setUI() {
         loginButton.layer.cornerRadius = loginButton.frame.height / 2
         loginButton.backgroundColor = UIColor(r: 223, g: 0, b: 23)
+        singInButton.addTarget(self, action: #selector(singInClick), for: .touchUpInside)
+        //加载注册页面
+        signUpViewOriginY = getStatusBarH()
+        view.addSubview(grayView)
+        view.addSubview(signInView)
+    }
+    
+    @objc func singInClick() {
+        viewAnimation(view: signInView, backView: grayView, backViewAlpha: 1, time: 0.5, y: getStatusBarH()/2)
+        self.perform(#selector(downMove), with: nil, afterDelay: 0.5)
+    }
+    
+    @objc func downMove() {
+        viewAnimation(view: signInView, backView: grayView, backViewAlpha: 1, time: 0.2, y: getStatusBarH())
+    }
+    
+    @objc func pan(panGuesture: UIPanGestureRecognizer) {
+        if panGuesture.state == .began {
+            print("开始拖动")
+        } else if panGuesture.state == .changed {
+            let y = panGuesture.translation(in: self.view).y
+            if y <= getStatusBarH() {
+                signInView.frame = CGRect(x: 0, y: getStatusBarH(), width: kScreenW, height: kScreenH)
+            } else {
+                
+                signInView.frame = CGRect(x: 0, y: signUpViewOriginY+y, width: kScreenW, height: kScreenH)
+            }
+            var scrolAlph = self.signInView.frame.origin.y/kScreenH/2
+            if scrolAlph > 0.5 {
+                scrolAlph = 0.5
+            }
+            self.grayView.alpha = 1-scrolAlph
+        } else if panGuesture.state == .ended {
+            if self.signInView.frame.origin.y <= kScreenH/2 {
+                viewAnimation(view: signInView, backView: grayView, backViewAlpha: 1, time: 0.2, y: signUpViewOriginY)
+            } else {
+                viewAnimation(view: signInView, backView: grayView, backViewAlpha: 0, time: 0.2, y: kScreenH)
+            }
+            
+        }
     }
 }
